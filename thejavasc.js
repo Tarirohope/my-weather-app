85b5ae81dc7c0bf706a8ef1f0cb8c2b1d333aff3
tarirohope <ptkpriscila@gmail.com>
2024-02-25 18:28:57 +0200

    I updated the js file
    I updated the js. css and html file

--- thejavasc.js
+++ thejavasc.js
@@ -1,13 +1,34 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector(".current-degrees");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = `${temperature}`;
+}
 function whileSearching(event) {
   event.preventDefault();
-  let theCity = document.querySelector(".search-input");
-
+  let theCity = document.querySelector("#current-city");
   let h1 = document.querySelector("h1");
   h1.innerHTML = `${theCity.value}`;
+  let city = `${theCity.value}`;
+
+  let apiKey = "438ef4abe978ad5a029db4f3900o6f4t";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayTemperature);
 }
 let searchForm = document.querySelector("form");
 searchForm.addEventListener("submit", whileSearching);
+
 function cityDate() {
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
   let days = [
     "Sunday",
     "Monday",
@@ -18,25 +39,11 @@ function cityDate() {
     "Saturday",
   ];
   let now = new Date();
-  let hour = now.getHours();
+  let hours = now.getHours();
   let minutes = now.getMinutes();
   let day = days[now.getDay()];
-  let mydate = `${day} ${hour}:${minutes}`;
+  let mydate = `${day} ${hours}:${minutes}`;
   let currentDay = document.querySelector("#current-date");
   currentDay.innerHTML = `${mydate}`;
 }
 cityDate();
-let cityElememnt = form;
-let apiKey = "438ef4abe978ad5a029db4f3900o6f4t";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-
-function displayTemperature(response) {
-  city = response.data.city;
-  let temperature = Math.round(response.data.temperature.current);
-  let country = response.data.country;
-  let description = response.data.condition.description;
-
-  let temperatureElement = document.querySelector("#now-temp");
-  temperatureElement.innerHTML = `${temperature}`;
-}
-axios.get(apiUrl).then(displayTemperature);
